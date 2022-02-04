36dcbdd96f832ac8fe2d605568353609e01bec0e
jorgepardor <jorgeluis.mail@gmail.com>
2022-02-04 18:28:00 +0000

    ejercicio completo, pero metiendo los valores de conversión a lo bestia

---
+++ app.js
@@ -0,0 +1,43 @@
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+
+
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;
+}
+
+// declaramos una funcion con el mismo nombre "fromDollarToYen"
+const fromDollarToYen = function(valueInDollar){
+    // convertimos el valor a dolares
+    let valueInYen = valueInDollar * 102.32;
+    // retornamos el valor
+    return valueInYen;
+}
+
+// declaramos una funcion con el mismo nombre "fromYenToPound"
+const fromYenToPound = function(valueInYen){
+    // convertimos el valor a dolares
+    let valueInPound = valueInYen * 0.00975;
+    // retornamos el valor
+    return valueInPound;
+}
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
